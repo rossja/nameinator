dbf6a32f9f7abbdd0f63e9d629ace9acb0cc6072
Jason Ross <algorythm@gmail.com>
2021-09-01 10:20:15 -0400

    basic working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const gen = require('project-name-generator')
+const animals = require('./models/animals.json').animals
+const chemicals = require('./models/chemicals.json').chemicals
+const colors = require('./models/colors.json').colors
+const sep = '-'
 
-exports.generateProjectName = function (req) {
-  return gen({
-    number: true,
-    words: 2
-  // alliterative: true
-  }).dashed
-}
+const color = colors[Math.floor(Math.random() * colors.length)]
+const animal = animals[Math.floor(Math.random() * animals.length)]
+const chemical = chemicals[Math.floor(Math.random() * chemicals.length)]
+const number = Math.floor(1000 + Math.random() * 9000);
+
+console.log(`${color}${sep}${chemical}${sep}${animal}${sep}${number}`)
