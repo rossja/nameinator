f2d93a6fb85c134168efce83594562a33e19bbea
Jason Ross <algorythm@gmail.com>
2021-09-01 10:38:56 -0400

    no chemicals

--- src/namegen.js
+++ src/namegen.js
@@ -6,9 +6,9 @@ const sep = '-'
 exports.createName = function (req) {
   const color = colors[Math.floor(Math.random() * colors.length)]
   const animal = animals[Math.floor(Math.random() * animals.length)]
-  const chemical = chemicals[Math.floor(Math.random() * chemicals.length)]
+  // const chemical = chemicals[Math.floor(Math.random() * chemicals.length)]
   const number = Math.floor(1000 + Math.random() * 9000)
-  //const projectName = `${color}${sep}${chemical}${sep}${animal}${sep}${number}`
+  // const projectName = `${color}${sep}${chemical}${sep}${animal}${sep}${number}`
   const projectName = `${color}${sep}${animal}${sep}${number}`
   return projectName
 }
