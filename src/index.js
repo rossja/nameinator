f1898c4f333fb1cc3b734cb8922d428629145592
Jason Ross <algorythm@gmail.com>
2021-09-01 10:20:37 -0400

    stupid semicolon

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const sep = '-'
 const color = colors[Math.floor(Math.random() * colors.length)]
 const animal = animals[Math.floor(Math.random() * animals.length)]
 const chemical = chemicals[Math.floor(Math.random() * chemicals.length)]
-const number = Math.floor(1000 + Math.random() * 9000);
+const number = Math.floor(1000 + Math.random() * 9000)
 
 console.log(`${color}${sep}${chemical}${sep}${animal}${sep}${number}`)
